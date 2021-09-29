fec5135261facb9f5b83ac45f7fd341208c39434
Alex Cannizzo <cansal89@gmail.com>
2021-09-29 17:03:31 +0700

    Initial Commit

---
+++ Favourite Music/Favourite_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Favourite_MusicApp.swift
+//  Favourite Music
+//
+//  Created by Alex Cannizzo on 29/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Favourite_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
